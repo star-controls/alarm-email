278c0d486bbd550a6a079f898a8a7d509f6fdeac
Jaroslav Adam <jaroslav.adam@cern.ch>
2018-07-02 12:59:59 -0400

    Bug fix in email sending

--- mail.py
+++ mail.py
@@ -3,29 +3,40 @@ import smtplib
 from email.MIMEMultipart import MIMEMultipart
 from email.MIMEText import MIMEText
 import pandas
+from datetime import datetime
 
 class mail(object):
    #_____________________________________________________________________________
    def __init__(self):
-      self.server = smtplib.SMTP('smtp.gmail.com', 587)
-      self.server.starttls()
-      self.login = pandas.read_csv("login.csv")
-      self.server.login(self.login["login"][0], self.login["pwd"][0])
+      self.login_file = "login.csv"
+      self.subscribers_file = "subscribers.csv"
 
    #_____________________________________________________________________________
    def put_msg(self, msg_text):
 
-      maillist = pandas.read_csv("subscribers.csv")
+      server = smtplib.SMTP('smtp.gmail.com', 587)
+      server.starttls()
+
+      login = pandas.read_csv(self.login_file)
+      server.login(login["login"][0], login["pwd"][0])
+
+      msg_time = ", time: " + str(datetime.now()).split(".")[0]
+      msg_text += msg_time
+
+      maillist = pandas.read_csv(self.subscribers_file)
       for imail in range(len(maillist)):
          msg = MIMEMultipart()
-         sendfrom = self.login["login"][0]
+         sendfrom = login["login"][0]
          sendto = maillist["address"][imail]
          msg['From'] = sendfrom
          msg['To'] = sendto
          msg['Subject'] = "STAR Alarm Handler"
          msg.attach(MIMEText(msg_text, 'plain'))
-         self.server.sendmail(sendfrom, sendto, msg.as_string())
+         server.sendmail(sendfrom, sendto, msg.as_string())
       print msg_text
+
+      server.quit()
+
 
 
 
